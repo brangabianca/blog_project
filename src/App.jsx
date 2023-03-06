633857d7f7ed463ac2a4be9a96b59c57075a7aa3
brangabianca <brangabianca@yahoo.com>
2023-03-06 20:27:37 +0200

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import Blogs from "./pages/Blogs";
+import Wishlist from "./pages/Wishlist";
+import Home from "./pages/Home";
+import Navigation from "./components/Navigation";
+
 function App() {
-	return <div>APP</div>;
+	return (
+		<div>
+			<Navigation />
+			<Routes>
+				<Route path='/home' element={<Home />} />
+				<Route path='/blogs' element={<Blogs />} />
+				{/* <Route path="/blog/:idBlog"/> */}
+				<Route path='/wishlist' element={<Wishlist />} />
+				<Route path='/*' element={<h1>NOT FOUND!</h1>} />
+			</Routes>
+			{/* FOOTER */}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/Blogs.jsx
@@ -0,0 +1,5 @@
+function Blogs() {
+	return <h1>Blogs pages</h1>;
+}
+
+export default Blogs;
